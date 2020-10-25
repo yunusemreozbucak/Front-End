e9396a44fe37895eb59d0dca7c10f74a9f37cc8d
Yunus <55831316+yunusozbucak@users.noreply.github.com>
2020-10-25 18:53:11 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,66 +1,25 @@
-import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import ResultComponent from './components/ResultComponent';
-import KeyPadComponent from "./components/KeyPadComponent";
 
-class App extends Component {
-    constructor(){
-        super(); //constructor çağırmak için çağırılır.
-        
-        this.state = {
-            result: ""
-        }
-    }
-onClick = button => {
-    if(button === "="){
-        this.calculate()
-    
-    }
-    else if(button === "C"){
-        this.reset()
-    }
-    
-    else if (button === "CE"){
-        this.backspace()
-    }
-    else {
-        this.setState({
-            result: this.state.result + button  
-        })
-    }
-};
-calculate = () =>{
-    try {
-        this.setState({
-            result: (eval(this.state.result) ||"") + ""
-        })
-    } catch (e){
-        this.setState({
-            result: "error"
-        })
-    }
-};
-reset = () => {
-    this.setState({
-        result: ""
-    })
-};
-backspace = () => {
-    this.setState({
-        result: this.state.result.slice(0,-1)
-    })
-}
-    render(){
-        return (
-            <div>
-                <div className="calculator-body">
-                    <h1>Simple Calculator</h1>
-                    <ResultComponent result={this.state.result}/>
-                    <KeyPadComponent onClick={this.onClick}/>
-                </div>
-            </div>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
